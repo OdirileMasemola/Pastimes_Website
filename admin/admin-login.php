10fca483672efa5d223e5f847342e1bac415180a
Odirile Masemola <odirilemasemola1@gmail.com>
2026-04-30 23:48:11 +0200

    Improve admin login error handling and password verification logic

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -127,11 +127,3 @@ $conn->close();
     </main>
 </body>
 </html>
-<?php
-/*
-This code is the original work of:
-ST10441421 - Odirile Masemola
-ST10450294 - Ripfumelo Mabasa
-All rights reserved.
-*/
-?>
